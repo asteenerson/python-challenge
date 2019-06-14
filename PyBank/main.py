38b46de5040c06289c7b284bf870dd637c72baaa
Aaron S <asteenerson@gmail.com>
2019-06-13 18:22:45 -0700

    Completed Month total and Net total

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,25 +5,47 @@ import csv
 # Path the CSV file
 budget_csv = os.path.join("budget_data.csv")
 
-# Read in the CSV file
+# Read the CSV file
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     # Skip Header
     next(csvreader)
-    
-    #Count the number of rows for total month count
+
+    # Count the number of rows for total month count
     total_months = list(csvreader)
 
+    # Read the CSV file
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')    
     
+    # Define net total
+    net_total = 0
+
+    # sum row1
+    for row in csvreader:
+        net_total = sum(int(row[1]) for row in csvreader)
+
+# Read the CSV file
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',') 
     
-    #for row in csvreader:
-        #total_months[row[0]].append(row)
-        #print(total_months)
+        #print(list1)
+
+list2 = []
+
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',') 
+    
+
+
+    #Test for regular Average
+    #average = (int(net_total) / len(total_months))
+    #print(average)
 
 
     # Final print of financial analysis
-    print("Financial Analysis")
-    print("----------------------------")
-    print(f'Total Months: {len(total_months)}')
-    
+    #print("Financial Analysis")
+    #print("----------------------------")
+    #print(f'Total Months: {len(total_months)}')
+    print(f'Total: ${net_total}')
